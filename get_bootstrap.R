59070dea46dd342dcd0992b3a33eb5db18c5d2bc
tredmill <erwin.gielens@gmail.com>
2019-09-23 13:26:10 +0200

    added files to package

--- get_bootstrap.R
+++ get_bootstrap.R
@@ -1,4 +1,24 @@
-get.bootstrap <- function(data, statistic, replicates, formula) {
+#' The title goes here.
+#'
+#' The subtitle goes here.
+#'
+#' A long description that spans over multiple rows is allowed right here. 
+#' Note that `roxygen2` documentation starts with `#'`. Also, spacing matters.
+#'
+#' @param data The argument type. Additional description for the argument.
+#' @param statistic The argument type. Additional description for the argument.
+#' @param replicates The argument type. Additional description for the argument.
+#' @param formula The argument type. Additional description for the argument.
+#'
+#' @return Description about the return type of the function. Similarly, the return 
+#' description can also span over multiple rows and the spacing matters.
+#'
+#' @examples
+#' \dontrun{
+#' test <- get.bootstrap(data = cars, statistic = get.lm, replicates = 1e4, formula = speed ~ dist)
+#' }
+#' @export
+get_bootstrap <- function(data, statistic, replicates, formula) {
     # Get the number of rows.
     n <- nrow(data)
  
---
+++ zzz.R
@@ -0,0 +1,15 @@
+#' Package title.
+#'
+#' @details
+#' Multi-line package description.
+#' 
+#' @section Section name:
+#' You can add multiple sections.
+#'
+#' @import grDevices
+#' @importFrom stats lm
+#'
+#' @docType package
+#'
+#' @name footstrap
+"_PACKAGE"
